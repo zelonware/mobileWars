d1eeab107f28f0715c1e898043175fd07ba570fd
Elena G Blanco <elena.guzbla@gmail.com>
2023-07-17 07:47:11 +0200

    Start moviesdb project

---
+++ moviesdb/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('moviesdb'),
+        ),
+        body: const Center(child: Text('This is your app')),
+      ),
+    );
+  }
+}
