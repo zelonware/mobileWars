7d0e813963fd0f246959c992d74bc96d5b224468
Elena G Blanco <elena.guzbla@gmail.com>
2023-08-10 22:16:26 +0200

    Start login page

--- blocform/lib/main.dart
+++ blocform/lib/main.dart
@@ -33,20 +33,40 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Lets learn BLoC',
-            ),
-          ],
-        ),
-      ),
+        body: Stack(
+      children: [
+        background(),
+        Container(
+          padding: const EdgeInsets.only(top: 80),
+          child: const Column(
+            children: [
+              Icon(
+                Icons.person_pin_circle,
+                color: Colors.white,
+                size: 100,
+              ),
+              SizedBox(
+                width: double.infinity,
+              ),
+              Text(
+                'My awesome app',
+                style: TextStyle(color: Colors.white, fontSize: 14),
+              )
+            ],
+          ),
+        )
+      ],
+    ));
+  }
+
+  Widget background() {
+    final size = MediaQuery.of(context).size;
+
+    return Container(
+      height: size.height * 0.4,
+      width: double.infinity,
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(colors: [Colors.blue, Colors.purple])),
     );
   }
 }
