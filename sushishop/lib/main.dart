5c085e7a3cdda5e17b19ee5bfaf3060a7524a7ca
Elena G Blanco <elena.guzbla@gmail.com>
2023-10-02 18:48:54 +0200

    Add home info

--- sushishop/lib/main.dart
+++ sushishop/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,12 +33,48 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Sushi shop'),
-      ),
-      body: const Center(
-        child: CircularProgressIndicator(),
+      backgroundColor: const Color.fromARGB(255, 130, 60, 55),
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 25,
+            ),
+            // name
+            Text('SUPER SUSHI',
+                style: GoogleFonts.dmSerifDisplay(
+                    color: Colors.white, fontSize: 22)),
+            const SizedBox(
+              height: 25,
+            ),
+            // icon
+            Padding(
+              padding: const EdgeInsets.all(50.0),
+              child: Image.asset('img/maki.png'),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            // title
+            Text(
+              'THE TASTE OF JAPANESE FOOD',
+              style:
+                  GoogleFonts.dmSerifDisplay(color: Colors.white, fontSize: 22),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            // subtitle
+            Text(
+                'Feel the taste of the most popular japanese food, its awesome!',
+                style: GoogleFonts.dmSerifDisplay(
+                    color: Colors.white, fontSize: 18)),
+
+            // get started
+          ],
+        ),
       ),
     );
   }
